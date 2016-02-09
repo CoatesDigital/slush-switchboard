b7374d4e0665bf082ebc7bf83ddf0ca57bc245c7
Shannon Archer <shannon.archer0@gmail.com>
2016-02-10 08:30:31 +1100

    update to javascript file to better format

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,12 +1,16 @@
 // Configure Switchboard
-SB.setup({
-	url: '',
-	sources: [
-		{ name: '', filename:'' }
-	],
-	success: init
-});
+(function () {
 
-function init() {
-	// Start your application logic here	
-}
+	function init() {
+		// Start your application logic here
+	}
+
+	SB.setup({
+		url: 'http://xxxx.coatesdigital.com.au/',
+		sources: [
+			{ name: 'sample', filename:'sample.csv' }
+		],
+		success: init
+	});
+
+})();
